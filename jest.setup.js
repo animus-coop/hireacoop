9abc010e46de09e83fd631d8de50924369f4cbfb
Juan Acosta <juan.acosta.rios@codigolibre.coop.ar>
2025-03-03 15:18:02 -0300

    Added more jest setup configs

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 
 global.render = render;
-global.screen = screen;
+global.fireEvent = fireEvent;
+global.waitFor = waitFor;
